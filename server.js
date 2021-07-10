428d9ae9084e0aa42a209e6190e69a5f74eb404d
Gautham-22 <gautham22poy@gmail.com>
2021-07-10 21:40:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+if(process.env.NODE_ENV !== "production") {   // value of NODE_ENV will be production when deployed to Heroku
+    require("dotenv").config();               // implies that we are using .env of local repo when not in production 
+}
+
+const express = require("express");
+const ejs = require("ejs");
+const expressEjsLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const expressRouter = require("./routes/router");
+
+const app = express();
+const PORT = process.env.PORT || 3000;  // process.env.PORT will be populated by Heroku
+
+mongoose.connect(process.env.DATABASE_URL,{   // DATABASE_URL will be set with mongoDB cluster in heroku
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+})
+.then(() => {
+    console.log("Connected to mongodb");
+})
+.catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+})
+
+app.set("view engine","ejs");
+app.set("layout","layouts/layout");
+
+app.use(express.static("public"));
+app.use(expressEjsLayouts);
+app.use("/",expressRouter);
+
+
+app.listen(PORT,() => {
+    console.log("Server started running...");
+})
