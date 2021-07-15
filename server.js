c250f8df78add78c5305fb3ecf8b12559625caf1
Gautham-22 <gautham22poy@gmail.com>
2021-07-15 18:28:53 +0530

    modified server.js

--- server.js
+++ server.js
@@ -2,51 +2,35 @@ if(process.env.NODE_ENV !== "production") {   // value of NODE_ENV will be produ
     require("dotenv").config();               // implies that we are using .env of local repo when not in production 
 }
 
-const express = require("express");
-const ejs = require("ejs");
-const expressEjsLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const indexRouter = require("./routes/index");
-const authorRouter = require("./routes/author");
+mongoose.connect(process.env.DATABASE_URL,{   // DATABASE_URL will be populated as mongo cluster by heroku when deployed 
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
+.then(() => {
+    console.log("Connected to mongodb");
+    const express = require("express");
+    const ejs = require("ejs");
+    const expressEjsLayouts = require("express-ejs-layouts");
+    const indexRouter = require("./routes/index");
+    const authorRouter = require("./routes/author");
 
-const app = express();
-const PORT = process.env.PORT || 3000;  // process.env.PORT will be populated by Heroku
+    const app = express();
+    const PORT = process.env.PORT || 3000;  // process.env.PORT will be populated by Heroku
+    app.set("view engine","ejs");
+    app.set("layout","layouts/layout");
 
-// mongoose.connect(process.env.DATABASE_URL,{ 
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-//     useFindAndModify:false
-// })
-// .then(() => {
-//     console.log("Connected to mongodb");
-// })
-// .catch((err) => {
-//     console.log(err.message);
-//     process.exit(1);
-// })
+    app.use(express.static("public"));
+    app.use(express.urlencoded({extended:true}));
+    app.use(expressEjsLayouts);
+    app.use("/",indexRouter);
+    app.use("/authors",authorRouter);
 
-const databaseConnection = async () => {
-    try {
-        let connection = await mongoose.connect(process.env.DATABASE_URL, {   // DATABASE_URL will be set with mongoDB cluster in heroku
-            useNewUrlParser: true,
-            useFindAndModify:false 
-        });
-        console.log("Connected to mongodb");
-    } catch(err) {
-        console.log(err);
-    }
-}
-databaseConnection();
-
-app.set("view engine","ejs");
-app.set("layout","layouts/layout");
-
-app.use(express.static("public"));
-app.use(express.urlencoded({extended:true}));
-app.use(expressEjsLayouts);
-app.use("/",indexRouter);
-app.use("/authors",authorRouter);
-
-app.listen(PORT,() => {
-    console.log("Server started running...");
+    app.listen(PORT,() => {
+        console.log("Server started running...");
+    })
+})
+.catch((err) => {
+    console.log(err.message);
+    process.exit(1);
 })
