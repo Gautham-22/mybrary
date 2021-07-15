eeac872c0c700ea31c4090d5d57c9c12bd21cc23
Gautham-22 <gautham22poy@gmail.com>
2021-07-15 19:43:25 +0530

    modified flow of routing

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 if(process.env.NODE_ENV !== "production") {   // value of NODE_ENV will be production when deployed to Heroku
     require("dotenv").config();               // implies that we are using .env of local repo when not in production 
 }
+const express = require("express");
+const ejs = require("ejs");
+const expressEjsLayouts = require("express-ejs-layouts");
+
+const app = express();
+const PORT = process.env.PORT || 3000;  // process.env.PORT will be populated by Heroku
+
+app.set("view engine","ejs");
+app.set("layout","layouts/layout");
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL,{   // DATABASE_URL will be populated as mongo cluster by heroku when deployed 
+let indexRouter, authorRouter;
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser:true,
     useFindAndModify:false
 })
-.then(() => {
-    console.log("Connected to mongodb");
-    const express = require("express");
-    const ejs = require("ejs");
-    const expressEjsLayouts = require("express-ejs-layouts");
-    const indexRouter = require("./routes/index");
-    const authorRouter = require("./routes/author");
-
-    const app = express();
-    const PORT = process.env.PORT || 3000;  // process.env.PORT will be populated by Heroku
-    app.set("view engine","ejs");
-    app.set("layout","layouts/layout");
-
-    app.use(express.static("public"));
-    app.use(express.urlencoded({extended:true}));
-    app.use(expressEjsLayouts);
-    app.use("/",indexRouter);
+.then(() => {           // makes sure that models are loaded and used after connection
+    console.log("Connected to mongodb"); 
+    indexRouter = require("./routes/index");           
+    authorRouter = require("./routes/author");
+    app.use("/",indexRouter);                           
     app.use("/authors",authorRouter);
-
-    app.listen(PORT,() => {
-        console.log("Server started running...");
-    })
 })
 .catch((err) => {
     console.log(err.message);
     process.exit(1);
+})
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(expressEjsLayouts);
+
+app.listen(PORT,() => {
+    console.log("Server started running...");
 })
